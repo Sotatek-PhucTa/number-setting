7b5279b02a1498f1bfbee74c2060aa8d23e3e694
phucta <phuc.ta@sotatek.com>
2023-12-22 11:18:42 +0700

    Get and set number

--- programs/number-setting/src/lib.rs
+++ programs/number-setting/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use std::borrow::BorrowMut;
 
 declare_id!("4givkSHuinrWG49gm8xs1xPmZ5E6E7haMFwxyLdw23Ct");
 
@@ -9,7 +10,81 @@ pub mod number_setting {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn initialize_account_state(ctx: Context<InitializeAccountState>) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        state.number = 0;
+        Ok(())
+    }
+
+    pub fn set_global_number(ctx: Context<SetGlobalNumber>, number: u64) -> Result<()> {
+        let global = &mut ctx.accounts.global;
+        global.number = number;
+        Ok(())
+    }
+
+    pub fn set_state_number(ctx: Context<SetStateNumber>, number: u64) -> Result<()> {
+        let state = &mut ctx.accounts.state;
+        state.number = number;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 8,
+        seeds = [b"global"],
+        bump,
+    )]
+    pub global: Account<'info, State>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeAccountState<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 8,
+        seeds = [b"state", signer.key().as_ref()],
+        bump,
+    )]
+    pub state: Account<'info, State>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SetStateNumber<'info> {
+    #[account(
+        mut,
+        seeds = [b"state", signer.key().as_ref()],
+        bump,
+    )]
+    pub state: Account<'info, State>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct SetGlobalNumber<'info> {
+    #[account(
+        mut,
+        seeds = [b"global"],
+        bump,
+    )]
+    pub global: Account<'info, State>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[account]
+pub struct State {
+    pub number: u64,
+}
--- tests/number-setting.ts
+++ tests/number-setting.ts
@@ -1,16 +1,69 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import {BN, Program} from "@coral-xyz/anchor";
 import { NumberSetting } from "../target/types/number_setting";
+import {assert} from "chai";
 
 describe("number-setting", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.NumberSetting as Program<NumberSetting>;
+  const [global] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("global")],
+        program.programId,
+  )
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initialize()
+        .accounts({ global})
+        .rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Set global state value", async () => {
+    // Add your test here.
+    const tx = await program.methods
+        .setGlobalNumber(new BN(42))
+        .accounts({ global})
+        .rpc();
+    console.log("Your transaction signature", tx);
+
+
+    const globalAccount = await program.account.state.fetch(global);
+    assert(globalAccount.number.toNumber() === 42);
+  });
+
+  it("Initialized account state", async () => {
+    // Add your test here.
+    const newWallet = anchor.web3.Keypair.generate();
+    await provider.connection.requestAirdrop(newWallet.publicKey, 10000000000);
+    const [state ] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("state"), newWallet.publicKey.toBuffer()],
+        program.programId,
+    );
+    await delay(5000);
+    const tx = await program.methods
+        .initializeAccountState()
+        .accounts({ state, signer: newWallet.publicKey })
+        .signers([newWallet])
+        .rpc();
+    console.log("Your transaction signature", tx);
+
+    await program.methods.setStateNumber(new BN(42))
+        .accounts({
+          state,
+          signer: newWallet.publicKey,
+        })
+        .signers([newWallet])
+        .rpc();
+
+    const stateAccount = await program.account.state.fetch(state);
+    assert(stateAccount.number.toNumber() === 42);
+  });
 });
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
